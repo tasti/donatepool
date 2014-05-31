f053eda6995d1dd16f8dcf3fb9380f353ded8db4
Tasti Zakarie <tasti.zakarie@gmail.com>
2014-05-31 15:14:42 -0400

    Add database

--- app.js
+++ app.js
@@ -1,19 +1,29 @@
 var express = require("express");
 var app = express();
 var pg = require('pg');
+var Bookshelf  = require('bookshelf');
 
-pg.connect(process.env.DATABASE_URL || "ec2-107-22-163-140.compute-1.amazonaws.com", function(err, client) {
-  if (err) throw err;
+Bookshelf.PG = Bookshelf.initialize({
+    client: 'pg',
+      connection: {
+        host     : 'ec2-107-22-163-140.compute-1.amazonaws.com',
+        user     : 'zkyhliyptxwwry',
+        password : 'weLWaLDvhh9gpxNBMSLiOE7PY_',
+        database : 'dea6fsu3t8b7ri',
+        charset  : 'UTF8_GENERAL_CI'
+    }
+});
 
-  var query = client.query('SELECT * FROM your_table');
+// elsewhere, to use the client:
+var Bookshelf = require('bookshelf').PG;
 
-  query.on('row', function(row) {
-    console.log(JSON.stringify(row));
-  });
+var User = Bookshelf.Model.extend({
+  tableName: 'users'
 });
 
 app.get('/', function(req, res) {
-  res.send('Hello Battlehack!');
+    console.log(process.env.DATABASE_URL);
+    res.send('Hello Battlehack!');
 });
 
 var port = Number(process.env.PORT || 5000);

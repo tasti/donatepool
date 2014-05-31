166df289ccdc8a2853170ebb727278242edc305b
Tasti Zakarie <tasti.zakarie@gmail.com>
2014-05-31 14:48:40 -0400

    Database setup

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
 var express = require("express");
 var app = express();
+var pg = require('pg');
+
+pg.connect(process.env.DATABASE_URL || "ec2-107-22-163-140.compute-1.amazonaws.com", function(err, client) {
+  if (err) throw err;
+
+  var query = client.query('SELECT * FROM your_table');
+
+  query.on('row', function(row) {
+    console.log(JSON.stringify(row));
+  });
+});
 
 app.get('/', function(req, res) {
-  res.send('Hello World!');
+  res.send('Hello Battlehack!');
 });
 
 var port = Number(process.env.PORT || 5000);

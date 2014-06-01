d4ead2ea484c600aad006774d782291ed0b6d980
Robert Gawdzik <rgawdzik@hotmail.com>
2014-05-31 22:17:37 -0400

    front page

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,12 @@
 var phonecatApp = angular.module('donatePoolApp', []);
 
 phonecatApp.controller('donatePoolCtrl', function ($scope) {
+  $scope.instructions = [
+    "Create or Join a Pool.",
+    "Add money to the donation pool, and choose your preferred local charity.",
+    "Predict the world cup to the best of your abilities.",
+    "Add friends to your pool, then wait for the cup to start!"
+  ];
   $scope.begin = true;
   $( ".main" ).fadeIn( 1000 );
 });

b90e65b1952d3d36c30d4080c256129e2eb1022b
Robert Gawdzik <rgawdzik@hotmail.com>
2014-05-31 22:24:03 -0400

    Fixed the alignment stuff

--- public/js/main.js
+++ public/js/main.js
@@ -2,8 +2,8 @@ var phonecatApp = angular.module('donatePoolApp', []);
 
 phonecatApp.controller('donatePoolCtrl', function ($scope) {
   $scope.instructions = [
-    "Create or Join a Pool.",
-    "Add money to the donation pool, and choose your preferred local charity.",
+    "Create or Join a Pool for the World Cup.",
+    "Add money to the donation pool, and choose a local charity.",
     "Predict the world cup to the best of your abilities.",
     "Add friends to your pool, then wait for the cup to start!"
   ];
